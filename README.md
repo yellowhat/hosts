bf10c4185de29be6e700996756144c5f8c8067d8
yellowhat <yellowhat46@gmail.com>
2025-01-03 17:57:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# hosts
+
+## How-to
+
+1. Generate new list
+
+    ```bash
+    make adlist
+    ```
+
+2. Run test:
+
+    ```bash
+    make test
+    ```
---
+++ config.js
@@ -0,0 +1,71 @@
+module.exports = {
+    extends: [
+        "config:recommended",
+        "docker:enableMajor",
+        ":disableRateLimiting",
+        "helpers:pinGitHubActionDigests"
+    ],
+    autodiscover: false,
+    repositories: [
+        "yellowhat/hosts"
+    ],
+    dependencyDashboard: true,
+    baseBranches: ["main"],
+    labels: ["renovate"],
+    separateMajorMinor: true,
+    lockFileMaintenance: {
+        enabled: true,
+    },
+    packageRules: [
+        {
+            matchManagers: ["github-actions"],
+            enabled: true,
+        },
+        {
+            matchUpdateTypes: ["minor", "patch", "pin", "digest"],
+            automerge: true,
+        },
+    ],
+    "github-actions": {
+        fileMatch: [".github/*/*.ya?ml$"],
+    },
+    customManagers: [
+        {
+            description: "Look for pip install",
+            customType: "regex",
+            fileMatch: [".*"],
+            matchStrings: [
+                "(pip|pip3)\\s+install\\s+(?<depName>[_\\-a-zA-Z0-9]+)(\\[.*\\])?==(?<currentValue>[\\w.]+)",
+            ],
+            datasourceTemplate: "pypi",
+        },
+        {
+            description: "Look for npm install",
+            customType: "regex",
+            fileMatch: [".*"],
+            matchStrings: [
+                "npm\\s+install\\s+(?:--global|-g)\\s+(?<depName>[@\\/_\\-a-zA-Z]+)@(?<currentValue>[\\w.]+)",
+            ],
+            datasourceTemplate: "npm",
+        },
+        {
+            description: "Look for docker://",
+            customType: "regex",
+            fileMatch: [".*"],
+            matchStrings: [
+                "uses\\s*:\\s*['\"]?docker:\\\/\\\/(?<depName>.*?):(?<currentValue>.*)(@(?<currentDigest>.*?))?['\"]?\\s*"
+            ],
+            datasourceTemplate: "docker",
+        },
+        {
+            description: "Comment",
+            customType: "regex",
+            fileMatch: [".*"],
+            matchStrings: [
+                "#\\s*renovate:\\s*datasource=(?<datasource>.*?)\\s+depName=(?<depName>.*?)\\s+(versioning=(?<versioning>.*?))?\\s*(ARG)?.*_(ver|VER)(=|:\\s+)['\"]?(?<currentValue>[\\w+\\.\\-]*)['\"]?",
+            ],
+            versioningTemplate:
+                "{{#if versioning}}{{{versioning}}}{{else}}ver{{/if}}",
+        },
+    ],
+};
