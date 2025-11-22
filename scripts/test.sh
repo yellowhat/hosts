3ab5295b9f5b12466a4f57e8c622f7e52c797fde
yellowhat <1692490+yellowhat@users.noreply.github.com>
2025-11-22 18:50:17 +0000

    Use blank.dnsforge.de

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,11 +3,14 @@
 set -euo pipefail
 
 docker run \
+    --name blocky-test \
     --interactive \
+    --tty \
     --rm \
     --network host \
-    --volume "$PWD:/data" \
+    --volume .:/data:z \
     --security-opt label=disable \
     --workdir /data \
+    --entrypoint ash \
     docker.io/alpine:latest \
-    sh -c "apk add bash bind-tools && /data/scripts/test_inside.sh"
+    -c "apk add bash bind-tools && /data/scripts/test_inside.sh"
