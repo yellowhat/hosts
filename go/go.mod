717746813381b1d7ecd55498c32127195baca86e
Vasco Gervasi <vasco.gervasi@alfatraining.de>
2026-03-16 22:16:57 +0100

    Initial commit

---
+++ go/go.mod
@@ -0,0 +1,9 @@
+module hosts
+
+go 1.26.0
+
+toolchain go1.26.1
+
+require golang.org/x/net v0.52.0
+
+require golang.org/x/text v0.35.0 // indirect
---
+++ scripts/test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Test if hostnames are blocked or not
+set -euo pipefail
+
+docker run \
+    --name blocky-test \
+    --interactive \
+    --rm \
+    --network host \
+    --volume .:/data:z \
+    --security-opt label=disable \
+    --workdir /data \
+    --entrypoint ash \
+    docker.io/alpine:latest \
+    -c "apk add bash bind-tools && /data/scripts/test_inside.sh"
